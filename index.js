47fb9a50d9b1f8646f84822096149f41021cbc1e
rudra812 <rudrabhanu.sahoo@gmail.com>
2017-04-08 11:16:02 +0530

    POST method changes

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
-app.get('/searching',function(req,res){
+
+var urlencoder = bodyParser.urlencoded({extended:false})
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.get('/searching',cors(), function(req,res){
     res.send("Hello World");
+});
+app.post('/login',cors(),function(req,res){
+  var user_id = req.body['user'];
+  var pwd = req.body['password']; 
+  if(user_id !== "614673"){
+      return res.status(400).send({
+          message: 'This is an error!'
+      });
+  }   
+  res.status(200).send({
+    message:"Login Successfull"
+  });
 });
 app.listen(8085,function(){    
 	console.log("Application is running on port 8085");
